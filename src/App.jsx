67c91d5ef1c1a23ab2ccec2e5f78b0038f4aaea9
Yuchao Wang <wangyuchao.404@bytedance.com>
2025-02-24 20:19:15 +0800

    feat: add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import Spline from "@splinetool/react-spline";
 import GiftCard from "./GiftCard";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  const handleSplineLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <div className="app">
+      {loading && (
+        <div className="loading-overlay">
+          <div className="loader"></div>
+          <div className="loading-text">Loading...</div>
+        </div>
+      )}
       <div className="spline-container">
-        <Spline scene="https://prod.spline.design/WxriJxRzOXSxPXgW/scene.splinecode" />
+        <Spline
+          scene="https://prod.spline.design/WxriJxRzOXSxPXgW/scene.splinecode"
+          onLoad={handleSplineLoad}
+        />
       </div>
-        <iframe
-          className="birthday-video"
-          width="560"
-          height="250"
-          src="https://www.youtube.com/embed/oAQmRO42UHc?si=qFDR161uSBmlpLiU"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+      <iframe
+        className="birthday-video"
+        width="560"
+        height="250"
+        src="https://www.youtube.com/embed/oAQmRO42UHc?si=qFDR161uSBmlpLiU"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
       <GiftCard />
     </div>
   );
--- src/GiftCard.jsx
+++ src/GiftCard.jsx
@@ -4,8 +4,10 @@ const GiftCard = () => {
   return (
     <div className="gift-card">
       <h1>Happy Birthday!</h1>
-      <p>To the most amazing girlfriend,</p>
+      <p>To my most amazing girlfriend,</p>
       <p>Wishing you a day filled with love and joy!</p>
+      <br/>
+      <p>steven</p>
     </div>
   );
 };
